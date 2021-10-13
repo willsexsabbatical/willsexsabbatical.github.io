a493373201293bd382998323b736f2a871f49e6e
mlcsec <Downloaderimplants0@protonmail.com>
2021-10-13 16:55:19 +0100

    update

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,48 @@
+/*var countDownDate = new Date("Nov 13, 2021 12:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  //document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  document.getElementById("pa3 br3 bg-dark-blue dib days").innerHTML = days;
+  document.getElementById("pa3 br3 bg-dark-blue dib hours").innerHTML = hours;
+  document.getElementById("pa3 br3 bg-dark-blue dib minutes").innerHTML = minutes;
+  document.getElementById("pa3 br3 bg-dark-blue dib seconds").innerHTML = seconds;
+
+
+  //var clock = document.getElementById(id);
+  //var daysSpan = clock.querySelector('.days');
+  //var hoursSpan = clock.querySelector('.hours');
+  //var minutesSpan = clock.querySelector('.minutes');
+  //var secondsSpan = clock.querySelector('.seconds');
+  //daysSpan.innerHTML = days;
+  //hoursSpan.innerHTML = hours;//('0' + hours).slice(-2);
+  //minutesSpan.innerHTML = minutes;//('0' + minutes).slice(-2);
+  //secondsSpan.innerHTML = seconds;//('0' + seconds).slice(-2);
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    //document.getElementById("demo").innerHTML = "EXPIRED";
+    alert("COUNTDOWN OVER");
+  }
+}, 1000);
+
+/*
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor(t / 1000 % 60);
@@ -38,14 +83,14 @@ function initializeClock(id, endtime) {
 }
 
 // example 15 days out
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+//var deadline = new Date(Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000);
 // people in different timezones will see different dates unless you use UTC
 // Searcn your timezone in UTC
 // then use this format:
-// var deadline = new Date(Date.parse('19 August 2017 19:15:00Z'));
+var deadline = new Date(Date.parse('19 August 2017 19:15:00Z'));
 
 if (new Date() > deadline) {
   alert('COUNTDOWN COMPLETE! \n Some Call to Action!!!');
 }
 
-initializeClock('clockdiv', deadline);
+initializeClock('clockdiv', deadline);*/
